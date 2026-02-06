568f3b4c3cdf56a8329f8465cfd3ef08af6a38ad
nrmk9r6m8p-arch <sarankumar.softwaredeveloper@gmail.com>
2026-02-06 16:19:04 +0530

    Abstraction program

---
+++ src/main/java/com/sathurakiri/main/Demo.java
@@ -0,0 +1,16 @@
+package com.sathurakiri.main;
+
+abstract  class Demo {
+//     int mtr = 30;
+//    int  str =20;
+//    void run(){
+//        System.out.println("car is running");
+//        System.out.println(str);
+
+  abstract void run();
+
+    }
+
+
+
+
---
+++ src/main/java/com/sathurakiri/main/Motor.java
@@ -0,0 +1,18 @@
+package com.sathurakiri.main;
+
+public class Motor extends  Demo {
+
+
+    @Override
+    void run() {
+        System.out.println(" abacsfd");
+    }
+    public static void main(String[] args){
+        Demo tyre = new Motor();
+        tyre.run();
+
+    }
+
+}
+
+
